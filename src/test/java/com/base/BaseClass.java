ace1c0b4d5fb7402393ba6b9f9e01c883e976955
Sahil-4736 <sahil4736@gmail.com>
2023-05-10 12:43:34 +0530

    Added method in 425th line of the base class

--- src/test/java/com/base/BaseClass.java
+++ src/test/java/com/base/BaseClass.java
@@ -422,7 +422,7 @@ return firstSelectedOption;
     	js.executeScript("arguments[0].scrollIntoView(true)",element);
     	
     }
-    public void clear19(WebElement element) {
+    public void clear97(WebElement element) {
 		elementVisibility(element);
 		element.clear();
 	}
